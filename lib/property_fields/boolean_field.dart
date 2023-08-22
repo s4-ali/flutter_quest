f041550fe588c19ef04c4d98c30a4ba3f9bf2f74
hassanraza80775 <kh80775@gmail.com>
2023-08-22 11:36:22 +0500

    #8: Resolved requested changes

--- lib/property_fields/boolean_field.dart
+++ lib/property_fields/boolean_field.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_quest/core/property_provider.dart';
 import 'package:flutter_quest/widgets/fields/boolean_field.dart';
 
-/// These are the inputs required for a certain field,
-/// for example title, max value, or any other property
-/// that we would like to set in the field.
 class BooleanPropertyParams extends PropertyParams<bool> {
   BooleanPropertyParams({
     required super.id,
@@ -13,8 +10,6 @@ class BooleanPropertyParams extends PropertyParams<bool> {
   });
 }
 
-/// Here we define the design of field and
-/// and provide the onChanged and current value params
 class BooleanPropertyField extends PropertyField<BooleanPropertyParams, bool> {
   BooleanPropertyField(super.provider, super.params);
 
@@ -26,17 +21,14 @@ class BooleanPropertyField extends PropertyField<BooleanPropertyParams, bool> {
       ) {
     return BooleanField(
       title: params.title,
-      onChanged: (value) {
-        onChanged(value);
-      },
+      onChanged: onChanged,
       initial: value,
     );
   }
 }
 
-/// Helper method that register a property field inside property provider
 extension BooleanFieldPropertyProvider on PropertyProvider {
-  bool obscureField({
+  bool booleanField({
     required String id,
     required String title,
     required bool initial,
--- lib/widgets/fields/boolean_field.dart
+++ lib/widgets/fields/boolean_field.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quest/widgets/boolean_field.dart';
 
 class BooleanField extends StatelessWidget {
   final String title;
@@ -15,10 +14,35 @@ class BooleanField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppSwitchField(
-      title: title,
-      initial: initial,
-      onChanged: onChanged
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title),
+        const SizedBox(
+          height: 6,
+        ),
+        SizedBox(
+          height: 20,
+          width: 36,
+          child: FittedBox(
+            fit: BoxFit.fill,
+            child: Switch(
+              trackOutlineColor: MaterialStateProperty.all(Colors.transparent),
+              thumbIcon: MaterialStateProperty.all(const Icon(Icons.circle_sharp, color: Colors.transparent,)),
+              activeColor: Colors.white,
+              activeTrackColor: const Color(0xFF3772FF),
+              inactiveTrackColor: const Color(0xFF36343b),
+              inactiveThumbColor: const Color(0xFF938F99),
+              value: initial,
+              onChanged: (value) {
+                onChanged(value);
+              },
+
+            ),
+          ),
+        ),
+      ],
     );
   }
 }

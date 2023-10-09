4da16bdec5bc2b9babb2b8cc1f07f0fba4e38f00
hassanraza80775 <kh80775@gmail.com>
2023-10-09 17:35:56 +0500

    #28: created vertical direction property_field

---
+++ lib/property_fields/vertical_direction_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quest/core/property_provider.dart';
+import 'package:flutter_quest/widgets/fields/vertical_direction_field.dart';
+
+class VerticalDirectionPropertyParams extends PropertyParams<VerticalDirection> {
+  VerticalDirectionPropertyParams({
+    required super.id,
+    super.initialValue,
+    required super.title,
+    required super.defaultValue,
+    super.isOptional,
+  });
+}
+
+class VerticalDirectionPropertyField
+    extends PropertyField<VerticalDirectionPropertyParams, VerticalDirection> {
+  VerticalDirectionPropertyField(super.provider, super.params);
+
+  @override
+  Widget build(
+    VerticalDirectionPropertyParams params,
+    Function(VerticalDirection) onChanged,
+    VerticalDirection value,
+  ) {
+    return VerticalDirectionField(
+      onChanged: onChanged,
+      value: value,
+    );
+  }
+}
+
+extension VerticalDirectionFieldPropertyProvider on PropertyProvider {
+  VerticalDirection? verticalDirectionField({
+    required String id,
+    required String title,
+    VerticalDirection? initialValue,
+    bool isOptional = true,
+    VerticalDirection defaultValue = VerticalDirection.down,
+  }) {
+    final params = VerticalDirectionPropertyParams(
+      id: id,
+      title: title,
+      isOptional: isOptional,
+      defaultValue: defaultValue,
+      initialValue: initialValue,
+    );
+    return VerticalDirectionPropertyField(
+      this,
+      params,
+    )();
+  }
+}

fb05b944998bda714d455455b7c53587b7816ff9
hassanraza80775 <kh80775@gmail.com>
2023-10-09 16:57:17 +0500

    #28: created text direction property_field

---
+++ lib/property_fields/text_direction_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quest/core/property_provider.dart';
+import 'package:flutter_quest/widgets/fields/text_direction_field.dart';
+
+class TextDirectionPropertyParams extends PropertyParams<TextDirection> {
+  TextDirectionPropertyParams({
+    required super.id,
+    super.initialValue,
+    required super.title,
+    required super.defaultValue,
+    super.isOptional,
+  });
+}
+
+class TextDirectionPropertyField
+    extends PropertyField<TextDirectionPropertyParams, TextDirection> {
+  TextDirectionPropertyField(super.provider, super.params);
+
+  @override
+  Widget build(
+    TextDirectionPropertyParams params,
+    Function(TextDirection) onChanged,
+    TextDirection value,
+  ) {
+    return TextDirectionField(
+      onChanged: onChanged,
+      value: value,
+    );
+  }
+}
+
+extension TextDirectionFieldPropertyProvider on PropertyProvider {
+  TextDirection? textDirectionField({
+    required String id,
+    required String title,
+    TextDirection? initialValue,
+    bool isOptional = true,
+    TextDirection defaultValue = TextDirection.ltr,
+  }) {
+    final params = TextDirectionPropertyParams(
+      id: id,
+      title: title,
+      isOptional: isOptional,
+      defaultValue: defaultValue,
+      initialValue: initialValue,
+    );
+    return TextDirectionPropertyField(
+      this,
+      params,
+    )();
+  }
+}

4c84c1cf1cb8738341d2e305d16bfe156b06d99e
hassanraza80775 <kh80775@gmail.com>
2023-10-10 16:33:19 +0500

    #30: implemented fields in drawer widget

--- lib/catalog/widgets/drawer.dart
+++ lib/catalog/widgets/drawer.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quest/property_fields/clip_field.dart';
+import 'package:flutter_quest/property_fields/color_field.dart';
 import 'package:flutter_quest/widgets/core/propery_builder.dart';
 import 'package:flutter_quest/property_fields/number_field.dart';
+import 'package:flutter_quest/property_fields/border_radius_field.dart';
 
 
 class DrawerPropertyExplorer extends StatelessWidget {
@@ -14,17 +17,24 @@ class DrawerPropertyExplorer extends StatelessWidget {
       widgetName: "Drawer",
       builder: (provider) {
         final width = provider.widthField();
+        final color = provider.colorField(id: "bgColor", title: "Background Color");
+        final surfaceColor = provider.colorField(id: "surfaceColor", title: "Surface Tint Color");
+        final elevation = provider.numberField(id: "elevation", title: "Elevation")?? 0.0;
+        final shadowColor = provider.colorField(id: "shadowColor", title: "Shadow Color");
+        final borderRadius = provider.borderRadiusField(id: "borderRadius", title: "Border Radius")?? BorderRadius.zero;
+        final clipBehavior = provider.clipField(id: "clipBehavior", title: "Clip Behavior");
+
         return Drawer(
           width: width,
-          backgroundColor: Colors.grey,
-          elevation: 10,
-          shadowColor: Colors.grey,
-          surfaceTintColor: Colors.greenAccent,
-          shape:const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(0.0)),
+          backgroundColor: color,
+          elevation: elevation.toDouble(),
+          shadowColor: shadowColor,
+          surfaceTintColor: surfaceColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadius,
             side: BorderSide.none,
           ),
-          clipBehavior: Clip.none,
+          clipBehavior: clipBehavior,
           semanticLabel: "Drawer",
           child: ListView(),
         );

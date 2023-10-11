451757b7212acda1cd42bafeb8944d4171220455
hassanraza80775 <kh80775@gmail.com>
2023-10-11 15:49:16 +0500

    #30: updated fields of container widget

--- lib/catalog/widgets/container.dart
+++ lib/catalog/widgets/container.dart
@@ -17,55 +17,55 @@ class ContainerPropertyExplorer extends StatelessWidget {
       builder: (provider) {
         final height = provider.heightField();
         final width = provider.widthField();
-        final alignment= provider.alignmentField(id: "alignment", title: "Alignment");
-        final margin = provider.edgeInsetsField(id: "margin",title: "Margin");
-        final padding = provider.edgeInsetsField(id: "padding",title: "Padding");
+        final alignment =
+            provider.alignmentField(id: "alignment", title: "Alignment");
+        final margin = provider.edgeInsetsField(id: "margin", title: "Margin");
+        final padding =
+            provider.edgeInsetsField(id: "padding", title: "Padding");
         final color = provider.colorField(id: "color", title: "Color");
-        final foregroundColor = provider.colorField(id: "fgColor", title: "Foreground Color");
-        final transform = provider.matrix4Field(id: "transform", title: "Transform");
-        final clipBehavior = provider.clipField(id: "clipBehavior", title: "Clip Behavior")?? Clip.none;
-        final minHeight = provider.heightField(id: "minHeight", title: "Minimum Height")?? 0.0;
-        final maxHeight = provider.heightField(id: "maxHeight", title: "Maximum Height")?? double.infinity;
-        final minWidth = provider.widthField(id: "minWidth", title: "Minimum Width")?? 0.0;
-        final maxWidth = provider.widthField(id: "maxWidth", title: "Maximum Width")?? double.infinity;
-        final transformAlignment= provider.alignmentField(id: "transformAlignment", title: "Transform Alignment");
+        final foregroundColor =
+            provider.colorField(id: "fgColor", title: "Foreground Color");
+        final transform =
+            provider.matrix4Field(id: "transform", title: "Transform");
+        final clipBehavior =
+            provider.clipField(id: "clipBehavior", title: "Clip Behavior") ??
+                Clip.none;
+        final minHeight =
+            provider.heightField(id: "minHeight", title: "Minimum Height") ??
+                0.0;
+        final maxHeight =
+            provider.heightField(id: "maxHeight", title: "Maximum Height") ??
+                double.infinity;
+        final minWidth =
+            provider.widthField(id: "minWidth", title: "Minimum Width") ?? 0.0;
+        final maxWidth =
+            provider.widthField(id: "maxWidth", title: "Maximum Width") ??
+                double.infinity;
+        final transformAlignment = provider.alignmentField(
+            id: "transformAlignment", title: "Transform Alignment");
 
-
-
-
-        return Row(
-          verticalDirection: VerticalDirection.up,
-          children: [
-            Column(
-              verticalDirection: VerticalDirection.up,
-              children: [
-                Container(
-                  height: height,
-                  width: width,
-                  alignment: alignment,
-                  margin: margin,
-                  padding: padding,
-                  transform: transform,
-                  decoration: BoxDecoration(
-                    color: color,
-                  ),
-                  foregroundDecoration:BoxDecoration(
-                    color: foregroundColor,
-                  ),
-                  clipBehavior: clipBehavior,
-                  constraints:BoxConstraints(
-                    minWidth: minWidth,
-                    maxWidth: maxWidth,
-                    minHeight: minHeight,
-                    maxHeight: maxHeight,
-                  ),
-                  transformAlignment: transformAlignment,
-                  child: Container(
-                  ),
-                ),
-              ],
-            ),
-          ],
+        return Container(
+          height: height,
+          width: width,
+          alignment: alignment,
+          margin: margin,
+          padding: padding,
+          transform: transform,
+          decoration: BoxDecoration(
+            color: color,
+          ),
+          foregroundDecoration: BoxDecoration(
+            color: foregroundColor,
+          ),
+          clipBehavior: clipBehavior,
+          constraints: BoxConstraints(
+            minWidth: minWidth,
+            maxWidth: maxWidth,
+            minHeight: minHeight,
+            maxHeight: maxHeight,
+          ),
+          transformAlignment: transformAlignment,
+          child: Container(),
         );
       },
     );

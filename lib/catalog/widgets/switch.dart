6a1dbfd67dd1b50666546badbb3791a4d867e248
hassanraza80775 <kh80775@gmail.com>
2023-10-21 16:35:42 +0500

    #32: updated stringfield name in all fields and widgets

--- lib/catalog/widgets/switch.dart
+++ lib/catalog/widgets/switch.dart
@@ -5,7 +5,7 @@ import 'package:flutter_quest/property_fields/color_field.dart';
 import 'package:flutter_quest/property_fields/icon_data_field.dart';
 import 'package:flutter_quest/property_fields/list_field.dart';
 import 'package:flutter_quest/property_fields/number_field.dart';
-import 'package:flutter_quest/property_fields/string_field_field.dart';
+import 'package:flutter_quest/property_fields/string_field.dart';
 import 'package:flutter_quest/widgets/core/propery_builder.dart';
 
 class SwitchPropertyExplorer extends StatelessWidget {

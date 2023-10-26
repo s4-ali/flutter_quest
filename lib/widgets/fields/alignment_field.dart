099143e9a6c8e1ca8592b412094d21b4faefd061
hassanraza80775 <kh80775@gmail.com>
2023-10-26 13:58:29 +0500

    #54: updated alignment ui

--- lib/widgets/fields/alignment_field.dart
+++ lib/widgets/fields/alignment_field.dart
@@ -21,56 +21,41 @@ class AlignmentField extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _AlignmentFieldState createState() => _AlignmentFieldState();
+  State<AlignmentField> createState() => _AlignmentFieldState();
 }
 
 class _AlignmentFieldState extends State<AlignmentField> {
-
   AlignmentOption selectedOption = AlignmentOption.center;
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        Container(
-          width: 318,
-          decoration: BoxDecoration(
-            color: const Color(0xFF211F26),
-            borderRadius: BorderRadius.circular(8),
-            border: Border.all(
-              color: Colors.white,
-              width: 1,
-            ),
-          ),
-          child: Column(
-            children: List.generate(3, (row) {
-              return Row(
-                children: List.generate(3, (col) {
-                  AlignmentOption option = AlignmentOption.values[row * 3 + col];
-                  return AlignmentFieldItem(
-                    option: option,
-                    onChanged: (alignment) {
-                      setState(() {
-                        widget.onChanged(alignment);
-                        if (selectedOption != option) {
-                          selectedOption = option; // Deselect if already selected.
-                        }
-                      });
-                    },
-                    isSelected: selectedOption == option,
-                  );
-                }),
-              );
-            }),
-          ),
-        ),
-      ],
+      children: List.generate(3, (row) {
+        return Row(
+          children: List.generate(3, (col) {
+            AlignmentOption option = AlignmentOption.values[row * 3 + col];
+            return Expanded(
+              child: AlignmentFieldItem(
+                option: option,
+                onChanged: (alignment) {
+                  setState(() {
+                    widget.onChanged(alignment);
+                    if (selectedOption != option) {
+                      selectedOption = option; // Deselect if already selected.
+                    }
+                  });
+                },
+                isSelected: selectedOption == option,
+              ),
+            );
+          }),
+        );
+      }),
     );
   }
 }
 
 class AlignmentFieldItem extends StatefulWidget {
-
   final AlignmentOption option;
   final void Function(Alignment) onChanged;
   final bool isSelected;
@@ -87,9 +72,7 @@ class AlignmentFieldItem extends StatefulWidget {
 }
 
 class _AlignmentFieldItemState extends State<AlignmentFieldItem> {
-
-  BorderRadius? borderRadius;
-  String text = "";
+  String alignmentName = "";
   Alignment? alignment;
 
   AlignmentOption? onHoverOption;
@@ -99,53 +82,49 @@ class _AlignmentFieldItemState extends State<AlignmentFieldItem> {
   Widget build(BuildContext context) {
     switch (widget.option) {
       case AlignmentOption.topLeft:
-        borderRadius = const BorderRadius.only(topLeft: Radius.circular(7));
-        text = "Top Left";
+        alignmentName = "Top Left";
         alignment = Alignment.topLeft;
         break;
       case AlignmentOption.topCenter:
-        text = "Top";
+        alignmentName = "Top";
         alignment = Alignment.topCenter;
         break;
       case AlignmentOption.topRight:
-        borderRadius = const BorderRadius.only(topRight: Radius.circular(7));
-        text = "Top Right";
+        alignmentName = "Top Right";
         alignment = Alignment.topRight;
         break;
       case AlignmentOption.centerLeft:
-        text = "Left";
+        alignmentName = "Left";
         alignment = Alignment.centerLeft;
         break;
       case AlignmentOption.center:
-        text = "Center";
+        alignmentName = "Center";
         alignment = Alignment.center;
         break;
       case AlignmentOption.centerRight:
-        text = "Right";
+        alignmentName = "Right";
         alignment = Alignment.centerRight;
         break;
       case AlignmentOption.bottomLeft:
-        borderRadius = const BorderRadius.only(bottomLeft: Radius.circular(7));
-        text = "Bottom Left";
+        alignmentName = "Bottom Left";
         alignment = Alignment.bottomLeft;
         break;
       case AlignmentOption.bottomCenter:
-        text = "Bottom";
+        alignmentName = "Bottom";
         alignment = Alignment.bottomCenter;
         break;
       case AlignmentOption.bottomRight:
-        borderRadius = const BorderRadius.only(bottomRight: Radius.circular(7));
-        text = "Bottom Right";
+        alignmentName = "Bottom Right";
         alignment = Alignment.bottomRight;
         break;
       default:
-        borderRadius = BorderRadius.circular(0);
         alignment = Alignment.center;
-        text = "";
+        alignmentName = "";
         break;
     }
 
     return InkWell(
+      hoverColor: Colors.transparent,
       onTap: () {
         setState(() {
           widget.onChanged(alignment!);
@@ -158,29 +137,35 @@ class _AlignmentFieldItemState extends State<AlignmentFieldItem> {
         });
       },
       child: Container(
-        padding: const EdgeInsets.only(bottom: 2),
-        margin: const EdgeInsets.all(0),
-        alignment: Alignment.center,
-        width: 105.3,
-        decoration: BoxDecoration(
-          color: widget.isSelected ? const Color(0xFF36343B): Colors.transparent,
-          border: Border.all(
-            color: widget.isSelected ? Colors.white : const Color(0xFF49454F),
-            width: 1,
+          padding: const EdgeInsets.only(bottom: 2),
+          margin: const EdgeInsets.all(1),
+          alignment: Alignment.center,
+          height: 35,
+          decoration: BoxDecoration(
+            color: widget.isSelected
+                ? const Color(0xFF0099FF)
+                : Colors.transparent,
+            border: Border.all(
+              color: widget.isSelected
+                  ? Colors.transparent
+                  : isHover
+                      ? const Color(0xFF0099FF)
+                      : const Color(0xFF35363A),
+              width: 1,
+            ),
+            borderRadius: const BorderRadius.all(Radius.circular(4)),
           ),
-          borderRadius: borderRadius,
-        ),
-        child: Center(
-          child: Text(
-            (widget.isSelected ||
-                    isHover == true && onHoverOption == widget.option)
-                ? text : "",
-            style: TextStyle(
-              color: widget.isSelected ? const Color(0xFFE6E0E9) : const Color(0xFF49454F),
+          child: Center(
+            child: Text(
+              alignmentName,
+              style: TextStyle(
+                color: widget.isSelected || isHover
+                    ? const Color(0xFFFFFFFF)
+                    : const Color(0xFF808080),
+              ),
             ),
           ),
         ),
-      ),
     );
   }
 }

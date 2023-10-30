c8dd4251d2b56b76e7d49dd4295f8429f3986413
hassanraza80775 <kh80775@gmail.com>
2023-10-30 15:02:59 +0500

    #44: main axis size ui created

--- lib/widgets/fields/main_axis_size_field.dart
+++ lib/widgets/fields/main_axis_size_field.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class MainAxisSizeField extends StatelessWidget {
+class MainAxisSizeField extends StatefulWidget {
   final void Function(MainAxisSize) onChanged;
   final MainAxisSize value;
 
@@ -11,9 +12,111 @@ class MainAxisSizeField extends StatelessWidget {
   });
 
   @override
+  State<MainAxisSizeField> createState() => _MainAxisSizeFieldState();
+}
+
+class _MainAxisSizeFieldState extends State<MainAxisSizeField> {
+
+  MainAxisSize selectedOption = MainAxisSize.max;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      fallbackHeight: 60,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        for (final maixAxis in MainAxisSize.values) ...[
+          MainAxisRadioButton(
+              type: maixAxis,
+              selectedOption: selectedOption,
+              onTap: (value) {
+                setState(() {
+                  selectedOption = value;
+                  widget.onChanged(value);
+                });
+              }),
+        ],
+      ],
+    );
+  }
+}
+
+class MainAxisRadioButton extends StatefulWidget {
+  final MainAxisSize type;
+  final MainAxisSize selectedOption;
+  final Function(MainAxisSize) onTap;
+
+  const MainAxisRadioButton({
+    Key? key,
+    required this.type,
+    required this.selectedOption,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  State<MainAxisRadioButton> createState() => _MainAxisRadioButtonState();
+}
+
+class _MainAxisRadioButtonState extends State<MainAxisRadioButton> {
+  MainAxisSize result = MainAxisSize.max;
+  String image = "start.svg";
+  bool isHover = false;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (widget.type) {
+      case MainAxisSize.max:
+        result = MainAxisSize.max;
+        image = "maximum.svg";
+        break;
+      case MainAxisSize.min:
+        result = MainAxisSize.min;
+        image = "minimum.svg";
+        break;
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: () {
+          widget.onTap(result);
+        },
+        child: MouseRegion(
+          onEnter: (v) {
+            setState(() {
+              isHover = true;
+            });
+          },
+          onExit: (v) {
+            setState(() {
+              isHover = false;
+            });
+          },
+          child: Container(
+            height: 30,
+            width: 30,
+            decoration: BoxDecoration(
+                color: widget.selectedOption == widget.type
+                    ? const Color(0xFF0099FF)
+                    : Colors.transparent,
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(4),
+                ),
+                border: Border.all(
+                  width: 1,
+                  color: isHover ? const Color(0xFF0099FF) : Colors.transparent,
+                )),
+            child: Center(
+              child: SvgPicture.asset(
+                "assets/$image",
+                color: widget.selectedOption == widget.type || isHover
+                    ? const Color(0xFFFFFFFF)
+                    : const Color(0xFF808080),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

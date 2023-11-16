72556984ab2b58e36d6c448728fa60b21686bdec
hassanraza80775 <kh80775@gmail.com>
2023-11-16 14:48:11 +0500

    #48: working on curve ui

--- lib/widgets/fields/curve_field.dart
+++ lib/widgets/fields/curve_field.dart
@@ -1,7 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_quest/widgets/radio_button.dart';
 
-class CurveField extends StatefulWidget {
+const values = [
+  Curves.bounceIn,
+  Curves.bounceInOut,
+  Curves.bounceOut,
+  Curves.decelerate,
+  Curves.ease,
+  Curves.easeIn,
+  Curves.easeInBack,
+  Curves.easeInCirc,
+  Curves.easeInCubic,
+  Curves.easeInExpo,
+  Curves.easeInOut,
+  Curves.easeInOutBack,
+  Curves.easeInOutCirc,
+  Curves.easeInOutCubic,
+  Curves.easeInOutCubicEmphasized,
+  Curves.easeInOutExpo,
+  Curves.easeInOutQuad,
+  Curves.easeInOutQuart,
+  Curves.easeInOutQuint,
+  Curves.easeInOutSine,
+  Curves.easeInQuad,
+  Curves.easeInQuint,
+  Curves.easeInSine,
+  Curves.easeInToLinear,
+  Curves.easeOut,
+  Curves.easeOutBack,
+  Curves.easeOutCirc,
+  Curves.easeOutCubic,
+  Curves.easeOutExpo,
+  Curves.easeOutQuad,
+  Curves.easeOutQuint,
+  Curves.easeOutSine,
+  Curves.elasticIn,
+  Curves.elasticInOut,
+  Curves.elasticOut,
+  Curves.fastEaseInToSlowEaseOut,
+  Curves.fastLinearToSlowEaseIn,
+  Curves.fastOutSlowIn,
+  Curves.linear,
+  Curves.linearToEaseOut,
+  Curves.slowMiddle,
+];
+
+extension on Curve{
+  String get iconPath{
+    switch (this){
+      case Curves.bounceIn:
+        return "";
+      case Curves.bounceInOut:
+        return "";
+      case Curves.bounceOut:
+        return "";
+      default:
+        return "";
+    }
+  }
+}
+
+class CurveField extends StatelessWidget {
   final void Function(Curve) onChanged;
   final Curve value;
 
@@ -12,131 +71,18 @@ class CurveField extends StatefulWidget {
   });
 
   @override
-  State<CurveField> createState() => _CurveFieldState();
-}
-
-class _CurveFieldState extends State<CurveField> {
-  Curve selectedOption = Curves.linear;
-
-
-  @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        for (final curve in Curves.values) ...[
-          MainAxisRadioButton(
-              type: curve,
-              selectedOption: selectedOption,
-              onTap: (value) {
-                setState(() {
-                  selectedOption = value;
-                  widget.onChanged(value);
-                });
-              }),
-        ],
-      ],
-    );
-  }
-}
-
-class MainAxisRadioButton extends StatefulWidget {
-  final Curve type;
-  final Curve selectedOption;
-  final Function(Curve) onTap;
-
-  const MainAxisRadioButton({
-    Key? key,
-    required this.type,
-    required this.selectedOption,
-    required this.onTap,
-  }) : super(key: key);
-
-  @override
-  State<MainAxisRadioButton> createState() => _MainAxisRadioButtonState();
-}
-
-class _MainAxisRadioButtonState extends State<MainAxisRadioButton> {
-  Curve result = Curves.;
-  String image = "boxFitFill";
-  bool isHover = false;
-
-  @override
-  Widget build(BuildContext context) {
-    switch (widget.type) {
-      case BoxFit.fill:
-        result = BoxFit.fill;
-        image = "boxFitFill.svg";
-        break;
-      case BoxFit.contain:
-        result = BoxFit.contain;
-        image = "boxFitContain.svg";
-        break;
-      case BoxFit.cover:
-        result = BoxFit.cover;
-        image = "boxFitCover.svg";
-        break;
-      case BoxFit.fitWidth:
-        result = BoxFit.fitWidth;
-        image = "boxFitWidthFit.svg";
-        break;
-      case BoxFit.fitHeight:
-        result = BoxFit.fitHeight;
-        image = "boxFitHeightFit.svg";
-        break;
-      case BoxFit.scaleDown:
-        result = BoxFit.scaleDown;
-        image = "boxFitScaleDown.svg";
-        break;
-      case BoxFit.none:
-        result = BoxFit.none;
-        image = "boxFitNone.svg";
-        break;
-    }
-
-    return Padding(
-      padding: const EdgeInsets.all(7.0),
-      child: InkWell(
-        onTap: () {
-          widget.onTap(result);
-        },
-        child: MouseRegion(
-          onEnter: (v) {
-            setState(() {
-              isHover = true;
-            });
-          },
-          onExit: (v) {
-            setState(() {
-              isHover = false;
-            });
-          },
-          child: Container(
-            height: 30,
-            width: 30,
-            decoration: BoxDecoration(
-                color: widget.selectedOption == widget.type
-                    ? const Color(0xFF0099FF)
-                    : Colors.transparent,
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(4),
-                ),
-                border: Border.all(
-                  width: 1,
-                  color: isHover ? const Color(0xFF0099FF) : Colors.transparent,
-                )),
-            child: Center(
-              child: SvgPicture.asset(
-                "assets/$image",
-                color: widget.selectedOption == widget.type || isHover
-                    ? const Color(0xFFFFFFFF)
-                    : const Color(0xFF808080),
-              ),
-            ),
+        for (final curves in values)
+          AppRadioButton(
+            isSelected: value == curves,
+            iconPath: curves.iconPath,
+            onSelected: () => onChanged(curves),
           ),
-        ),
-      ),
+      ],
     );
   }
 }

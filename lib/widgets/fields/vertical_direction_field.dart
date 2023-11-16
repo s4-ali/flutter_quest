f492308d99c715b523085bd48b59fbf64913ca18
hassanraza80775 <kh80775@gmail.com>
2023-11-16 11:39:08 +0500

    #46: vertical direction updated

--- lib/widgets/fields/vertical_direction_field.dart
+++ lib/widgets/fields/vertical_direction_field.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_quest/widgets/radio_button.dart';
 
-class VerticalDirectionField extends StatefulWidget {
+extension on VerticalDirection {
+  String get iconPath {
+    switch (this) {
+      case VerticalDirection.down:
+        return "assets/downDirection.svg";
+      case VerticalDirection.up:
+        return "assets/upDirection.svg";
+      default:
+        return "";
+    }
+  }
+}
+
+class VerticalDirectionField extends StatelessWidget {
   final void Function(VerticalDirection) onChanged;
   final VerticalDirection value;
 
@@ -12,112 +25,18 @@ class VerticalDirectionField extends StatefulWidget {
   });
 
   @override
-  State<VerticalDirectionField> createState() => _VerticalDirectionFieldState();
-}
-
-class _VerticalDirectionFieldState extends State<VerticalDirectionField> {
-
-  VerticalDirection selectedOption = VerticalDirection.down;
-
-  @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        for (final maixAxis in VerticalDirection.values) ...[
-          VerticalDirectionRadioButton(
-              type: maixAxis,
-              selectedOption: selectedOption,
-              onTap: (value) {
-                setState(() {
-                  selectedOption = value;
-                  widget.onChanged(value);
-                });
-              }),
-        ],
-      ],
-    );
-  }
-
-}
-
-class VerticalDirectionRadioButton extends StatefulWidget {
-  final VerticalDirection type;
-  final VerticalDirection selectedOption;
-  final Function(VerticalDirection) onTap;
-
-  const VerticalDirectionRadioButton({
-    Key? key,
-    required this.type,
-    required this.selectedOption,
-    required this.onTap,
-  }) : super(key: key);
-
-  @override
-  State<VerticalDirectionRadioButton> createState() => _VerticalDirectionRadioButtonState();
-}
-
-class _VerticalDirectionRadioButtonState extends State<VerticalDirectionRadioButton> {
-  VerticalDirection result = VerticalDirection.down;
-  String image = "downDirection.svg";
-  bool isHover = false;
-
-  @override
-  Widget build(BuildContext context) {
-    switch (widget.type) {
-      case VerticalDirection.down:
-        result = VerticalDirection.down;
-        image = "downDirection.svg";
-        break;
-      case VerticalDirection.up:
-        result = VerticalDirection.up;
-        image = "upDirection.svg";
-        break;
-    }
-
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: InkWell(
-        onTap: () {
-          widget.onTap(result);
-        },
-        child: MouseRegion(
-          onEnter: (v) {
-            setState(() {
-              isHover = true;
-            });
-          },
-          onExit: (v) {
-            setState(() {
-              isHover = false;
-            });
-          },
-          child: Container(
-            height: 30,
-            width: 30,
-            decoration: BoxDecoration(
-                color: widget.selectedOption == widget.type
-                    ? const Color(0xFF0099FF)
-                    : Colors.transparent,
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(4),
-                ),
-                border: Border.all(
-                  width: 1,
-                  color: isHover ? const Color(0xFF0099FF) : Colors.transparent,
-                )),
-            child: Center(
-              child: SvgPicture.asset(
-                "assets/$image",
-                color: widget.selectedOption == widget.type || isHover
-                    ? const Color(0xFFFFFFFF)
-                    : const Color(0xFF808080),
-              ),
-            ),
+        for (final direction in VerticalDirection.values)
+          AppRadioButton(
+            iconPath: direction.iconPath,
+            isSelected: value == direction,
+            onSelected: () => onChanged(direction),
           ),
-        ),
-      ),
+      ],
     );
   }
 }

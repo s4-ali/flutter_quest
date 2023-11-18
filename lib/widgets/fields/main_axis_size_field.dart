a8f675f44136a991efce228d2f696314134257b7
hassanraza80775 <kh80775@gmail.com>
2023-11-18 12:43:14 +0500

    #44: text_direction updated

--- lib/widgets/fields/main_axis_size_field.dart
+++ lib/widgets/fields/main_axis_size_field.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quest/widgets/radio_button.dart';
+import 'package:flutter_quest/widgets/icon_options.dart';
 
 extension on MainAxisSize{
   String get iconPath{
@@ -26,17 +26,11 @@ class MainAxisSizeField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        for (final axisSize in MainAxisSize.values)
-          AppRadioButton(
-            isSelected: value == axisSize,
-            iconPath: axisSize.iconPath,
-            onSelected: () => onChanged(axisSize),
-          ),
-      ],
+    return IconOptions(
+      onChanged: (dynamic val) => onChanged(val as MainAxisSize),
+      value: value,
+      options: MainAxisSize.values,
+      iconPath: (dynamic value) => (value as MainAxisSize).iconPath,
     );
   }
 }

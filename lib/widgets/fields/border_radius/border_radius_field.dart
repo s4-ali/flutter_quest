600babc46664ee728816410e6d168ef7e80f71af
hassanraza80775 <kh80775@gmail.com>
2023-11-20 16:08:53 +0500

    #37: border radius creating

---
+++ lib/widgets/fields/border_radius/border_radius_field.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+enum BorderRadiusType {
+  all,
+  circular,
+  vertical,
+  horizontal,
+  only,
+  zero,
+}
+
+class BorderRadiusField extends StatefulWidget {
+  final void Function(BorderRadius) onChanged;
+  final BorderRadius value;
+
+  const BorderRadiusField({
+    super.key,
+    required this.onChanged,
+    required this.value,
+  });
+
+  @override
+  State<BorderRadiusField> createState() => _BorderRadiusFieldState();
+}
+
+class _BorderRadiusFieldState extends State<BorderRadiusField> {
+  BorderRadiusType selectedOption = BorderRadiusType.all;
+  late Widget selectedLayout;
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (v) {
+        setState(() {
+          isHovered = true;
+        });
+      },
+      onExit: (v) {
+        setState(() {
+          isHovered = false;
+        });
+      },
+      child: Container(
+        height: 30,
+        width: 134,
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          borderRadius: const BorderRadius.all(Radius.circular(6.0)),
+       //   border: buttonBorder,
+        ),
+        child: DropdownButton<BorderRadiusType>(
+          icon: const Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Icon(Icons.keyboard_arrow_down),
+          ),
+          iconSize: 12,
+          iconEnabledColor: const Color(0xFFFFFFFF),
+          focusColor: Colors.transparent,
+          underline: const SizedBox(),
+          alignment: Alignment.centerLeft,
+          value: selectedOption,
+          onChanged: (value) {
+            setState(() {
+              selectedOption = value!;
+            });
+          },
+          items: [
+            DropdownMenuItem<BorderRadiusType>(
+              onTap: () {
+                selectedLayout = const SizedBox();
+              },
+              value: BorderRadiusType.all,
+              child: const Padding(
+                padding: EdgeInsets.only(left: 8, bottom: 3),
+                child: Text("All"),
+              ),
+            ),
+            DropdownMenuItem<BorderRadiusType>(
+              onTap: () {
+                selectedLayout = const SizedBox();
+              },
+              value: BorderRadiusType.circular,
+              child: const Padding(
+                padding: EdgeInsets.only(left: 8, bottom: 5),
+                child: Text("Symmetric"),
+              ),
+            ),
+            DropdownMenuItem<BorderRadiusType>(
+              onTap: () {
+                selectedLayout = const SizedBox();
+              },
+              value: BorderRadiusType.vertical,
+              child: const Padding(
+                padding: EdgeInsets.only(left: 8, bottom: 5),
+                child: Text("Only"),
+              ),
+            ),
+            DropdownMenuItem<BorderRadiusType>(
+              onTap: () {
+                selectedLayout = const SizedBox();
+              },
+              value: BorderRadiusType.horizontal,
+              child: const Padding(
+                padding: EdgeInsets.only(left: 8, bottom: 5),
+                child: Text("Only"),
+              ),
+            ),
+            DropdownMenuItem<BorderRadiusType>(
+              onTap: () {
+                selectedLayout = const SizedBox();
+              },
+              value: BorderRadiusType.only,
+              child: const Padding(
+                padding: EdgeInsets.only(left: 8, bottom: 5),
+                child: Text("Only"),
+              ),
+            ),
+            DropdownMenuItem<BorderRadiusType>(
+              onTap: () {
+                selectedLayout = const SizedBox();
+              },
+              value: BorderRadiusType.zero,
+              child: const Padding(
+                padding: EdgeInsets.only(left: 8, bottom: 5),
+                child: Text("Only"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

a781f346b8d35c1d8805e3e69dc9f9a5740c8c83
Asfar Ali <30603432+s4-ali@users.noreply.github.com>
2023-11-28 12:28:42 +0500

    #37: Updated color field

--- lib/widgets/fields/color_field.dart
+++ lib/widgets/fields/color_field.dart
@@ -5,123 +5,155 @@ import 'package:flutter_quest/widgets/text_field.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
 
 List<Color> _commonColorList = [
-  const Color(0xFF1708FF),
-  const Color(0xFF000000),
-  const Color(0xFFFFFFFF),
-  const Color(0xFFFF0000),
-  const Color(0xFFFF7A00),
-  const Color(0xFF008000),
-  const Color(0xFFFFF000),
-  const Color(0xFF00FFB2),
-  const Color(0xFF4b0082),
-  const Color(0xFF8FFF00),
-  const Color(0xFFE22EFF),
-  const Color(0xFF545454),
-  const Color(0xFF7B0000),
-  const Color(0xFF00FF1A),
-  const Color(0xFF00E0FF),
+  const Color(0xFFF44236),
+  const Color(0xFFE91E63),
+  const Color(0xFF9C27B0),
+  const Color(0xFF6739B7),
+  const Color(0xFF3F51B5),
+  const Color(0xFF2396F3),
+  const Color(0xFF04A9F4),
+  const Color(0xFF00BCD4),
+  const Color(0xFF039588),
+  const Color(0xFF4CAE50),
+  const Color(0xFF8BC34A),
+  const Color(0xFFCCDC39),
+  const Color(0xFFFFEB3B),
+  const Color(0xFFFFC107),
+  const Color(0xFFFE9700),
+  const Color(0xFFFF5622),
+  const Color(0xFF795548),
+  const Color(0xFF617C8B),
+  const Color(0xFF617C8B),
+  const Color(0xFF9E9E9E),
 ];
 
-class ColorField extends StatefulWidget {
-  final void Function(Color) onChanged;
-  final Color value;
-
+class ColorField extends PropertyWidget<Color> {
   const ColorField({
     super.key,
-    required super.onChanged,
     required super.value,
+    required super.onChanged,
   });
 
   @override
-  State<ColorField> createState() => _ColorFieldState();
+  Widget build(BuildContext context) {
+    return _ColorField(onChanged: onChanged, value: value);
+  }
 }
 
-class _ColorFieldState extends State<ColorField> {
-  Color selectedColor = Colors.black;
-  int opacity = 100;
-  TextEditingController opacityTextEditingController =
-      TextEditingController(text: "100");
-  String textFieldValue = "000000";
-  bool flexColorDialog = false;
+class _ColorField extends StatelessWidget {
+  final void Function(Color) onChanged;
+  final Color value;
 
-  void updateColor(Color color) {
-    setState(() {
-      selectedColor = color;
-      widget.onChanged(selectedColor.withOpacity(opacity / 100));
-      textFieldValue = color.value.toRadixString(16).substring(2).toUpperCase();
-    });
-  }
+  _ColorField({
+    required this.onChanged,
+    required this.value,
+  });
+
+  final TextEditingController opacityTextEditingController =
+      TextEditingController(text: "100");
 
   @override
   Widget build(BuildContext context) {
+    final textFieldValue =
+        value.value.toRadixString(16).substring(2).toUpperCase();
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Stack(children: [
-          SizedBox(
-            height: 30,
-            child: AppTextField(
-              controller:
-                  TextEditingController(text: textFieldValue.toString()),
-              contentPadding: const EdgeInsets.only(left: 35, bottom: 10),
-              suffixIcon: Container(
-                width: 52,
-                decoration: const BoxDecoration(
-                  border: Border(
-                    left: BorderSide(
-                      width: 1,
-                      color: Color(0xff35363A),
-                    ),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 6.8),
-                  child: TextField(
-                    controller: opacityTextEditingController,
-                    onChanged: (value) {
-                      opacity = int.parse(value);
-                      updateColor(selectedColor);
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: _ColorOptionBox(
+                fillColor: value,
+                onUpdated: (color) async {
+                  final newColor = await showColorPickerDialog(
+                    context,
+                    color,
+                    pickersEnabled: {
+                      ColorPickerType.wheel: true,
+                      ColorPickerType.accent: false,
+                      ColorPickerType.primary: false,
                     },
-                    textAlign: TextAlign.right,
-                    textAlignVertical: TextAlignVertical.center,
-                    maxLines: 1,
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      suffix: Padding(
-                        padding: EdgeInsets.only(right: 5),
-                        child: Text("%"),
+                    title: const Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Text(
+                        "Pick Color",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    enableShadesSelection: false,
+                    padding: const EdgeInsets.only(
+                        bottom: 32, left: 16, right: 16, top: 16),
+                    actionButtons: const ColorPickerActionButtons(
+                      okButton: true,
+                      closeButton: true,
+                      dialogActionButtons: false,
+                      dialogActionIcons: true,
+                    ),
+                    backgroundColor: const Color(0xFF000000),
+                  );
+                  onChanged(newColor);
+                },
+              ),
+            ),
+            Expanded(
+              child: SizedBox(
+                height: 30,
+                child: AppTextField(
+                  controller: TextEditingController(text: textFieldValue),
+                  contentPadding: const EdgeInsets.only(left: 8),
+                  suffixIcon: Container(
+                    width: 65,
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        left: BorderSide(
+                          width: 1,
+                          color: Color(0xff35363A),
+                        ),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 7.5),
+                      child: TextField(
+                        controller: opacityTextEditingController,
+                        onChanged: (value) {
+                          try {
+                            final intValue = int.parse(value);
+                            if (intValue > 100) {
+                              opacityTextEditingController.text = "100";
+                              return;
+                            }
+                            onChanged(this.value.withOpacity(intValue / 100));
+                          } catch (e) {
+                            opacityTextEditingController.text = "100";
+                          }
+                        },
+                        textAlign: TextAlign.right,
+                        textAlignVertical: TextAlignVertical.center,
+                        maxLines: 1,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          isDense: true,
+                          suffix: Padding(
+                            padding: EdgeInsets.only(right: 8),
+                            child: Text("%"),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              onChanged: (value) => updateColor(Color(int.parse("0xFF$value"))),
-            ),
-          ),
-          Positioned(
-            top: 5,
-            left: 7,
-            child: InkWell(
-              onTap: () {
-                setState(() {
-                  flexColorDialog = !flexColorDialog;
-                });
-              },
-              child: Container(
-                height: 20,
-                width: 20,
-                decoration: BoxDecoration(
-                  color: selectedColor,
-                  borderRadius: const BorderRadius.all(Radius.circular(4)),
-                  border: Border.all(
-                    color: const Color(0xFFFFFFFF).withOpacity(0.3),
-                    width: 1,
+                  onChanged: (value) => onChanged(
+                    Color(int.parse("0xFF$value")),
                   ),
                 ),
               ),
             ),
-          ),
-        ]),
+          ],
+        ),
         const Padding(
           padding: EdgeInsets.only(top: 16, bottom: 7),
           child: Text("Common Colors"),
@@ -130,59 +162,75 @@ class _ColorFieldState extends State<ColorField> {
           children: List.generate(
             _commonColorList.length,
             (index) => Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: _CommonColorContainer(
+              padding: const EdgeInsets.all(2.0),
+              child: _ColorOptionBox(
                 fillColor: _commonColorList[index],
-                onTab: updateColor,
+                isSelected: _commonColorList[index] == value,
+                onUpdated: onChanged,
               ),
             ),
           ),
         ),
-        flexColorDialog
-            ? Center(
-                child: ColorPicker(
-                  onColorChanged: updateColor,
-                  wheelSquarePadding: 15,
-                  color: selectedColor,
-                  wheelDiameter: 200,
-                  maxRecentColors: 5,
-                  enableShadesSelection: false,
-                  pickersEnabled: const <ColorPickerType, bool>{
-                    ColorPickerType.wheel: true,
-                    ColorPickerType.primary: false,
-                    ColorPickerType.accent: false,
-                  },
-                ),
-              )
-            : const SizedBox(),
       ],
     );
   }
 }
 
-class _CommonColorContainer extends StatelessWidget {
-  final Function(Color) onTab;
+class _ColorOptionBox extends StatefulWidget {
+  final Function(Color)? onUpdated;
   final Color fillColor;
 
-  const _CommonColorContainer({
+  final bool isSelected;
+
+  const _ColorOptionBox({
     required this.fillColor,
-    required this.onTab,
+    this.isSelected = false,
+    this.onUpdated,
   });
+
+  @override
+  State<_ColorOptionBox> createState() => _ColorOptionBoxState();
+}
+
+class _ColorOptionBoxState extends State<_ColorOptionBox> {
+  bool isHovering = false;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () => onTab(fillColor),
+      onTap: () => widget.onUpdated?.call(widget.fillColor),
+      onHover: (value) {
+        setState(() {
+          isHovering = value;
+        });
+      },
       child: Container(
-        height: 20,
-        width: 20,
+        height: 30,
+        width: 30,
         decoration: BoxDecoration(
-            color: fillColor,
-            borderRadius: const BorderRadius.all(Radius.circular(4)),
-            border: Border.all(
-              color: const Color(0xFFFFFFFF).withOpacity(0.3),
-              width: 1,
-            )),
+          color: widget.fillColor,
+          borderRadius: const BorderRadius.all(Radius.circular(8)),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+            color: isHovering
+                ? ((widget.fillColor.computeLuminance() > 0.179)
+                        ? Colors.black
+                        : Colors.white)
+                    .withOpacity(0.3)
+                : Colors.transparent,
+          ),
+          child: widget.isSelected
+              ? const Center(
+                  child: Icon(
+                    Icons.check,
+                    color: Colors.white,
+                    size: 16,
+                  ),
+                )
+              : const SizedBox(),
+        ),
       ),
     );
   }

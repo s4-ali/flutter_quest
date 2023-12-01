ca211daa8c818fc0237791430e442f48cd6af558
hassanraza80775 <kh80775@gmail.com>
2023-12-01 11:56:01 +0500

    #34: RangePicker UI created

--- lib/widgets/fields/range_picker_number_field.dart
+++ lib/widgets/fields/range_picker_number_field.dart
@@ -3,7 +3,6 @@ import 'package:flutter_quest/widgets/core/property.dart';
 import 'package:flutter_quest/widgets/core/property_previewer.dart';
 
 class RangePickerNumberField extends PropertyWidget<num> {
-
   const RangePickerNumberField({
     super.key,
     required super.onChanged,
@@ -12,8 +11,60 @@ class RangePickerNumberField extends PropertyWidget<num> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      fallbackHeight: 60,
+    return Row(
+      children: [
+        _AppRangePicker(),
+      ],
+    );
+  }
+}
+
+class _AppRangePicker extends StatefulWidget {
+  @override
+  _AppRangePickerState createState() => _AppRangePickerState();
+}
+
+class _AppRangePickerState extends State<_AppRangePicker> {
+  double selectedValue = 50.0;
+  int numberOfDots = 7;
+  Color thumbColor = const Color(0xFF808080);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        MouseRegion(
+          onEnter: (v) {
+            setState(() {
+              thumbColor = const Color(0xFFFFFFFF);
+            });
+          },
+          onExit: (v) {
+            setState(() {
+              thumbColor = const Color(0xFF808080);
+            });
+          },
+          child: Slider(
+            activeColor: Color(0xFFFFFFFF),
+            inactiveColor: Color(0xFF808080),
+            thumbColor: thumbColor,
+            secondaryActiveColor: Colors.black,
+            secondaryTrackValue: 1,
+            value: selectedValue,
+            onChanged: (value) {
+              setState(() {
+                selectedValue = value;
+              });
+            },
+            min: 0,
+            max: 100,
+            divisions: 10,
+            label: selectedValue.round().toString(),
+          ),
+        ),
+        Text('Selected Value: ${selectedValue.round()}'),
+      ],
     );
   }
 }

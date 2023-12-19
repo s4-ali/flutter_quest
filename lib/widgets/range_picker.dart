ce4dee1e303c8f8cd8b0a64eb1203f1e5ca72ca9
hassanraza80775 <kh80775@gmail.com>
2023-12-19 10:02:26 +0500

    #34: Range picker UI created

--- lib/widgets/range_picker.dart
+++ lib/widgets/range_picker.dart
@@ -8,9 +8,13 @@ class AppRangePicker extends StatefulWidget {
   final double max;
   final double stepSize;
 
-
-  const AppRangePicker(
-      {super.key, required this.onChanged, this.min = 0, this.max = 100, this.stepSize = 1});
+  const AppRangePicker({
+    super.key,
+    required this.onChanged,
+    this.min = 0,
+    this.max = 100,
+    this.stepSize = 1,
+  });
 
   @override
   State<AppRangePicker> createState() => _AppRangePickerState();
@@ -60,18 +64,18 @@ class _AppRangePickerState extends State<AppRangePicker> {
             value: selectedValue,
             activeColor: const Color(0xFFFFFFFF),
             inactiveColor: const Color(0xFF35363A),
-            trackShape: const SfTrackShape(),
+            trackShape: _SfTrackShape(),
             interval: 10,
             showDividers: true,
             stepSize: 1,
-            dividerShape: const SfDividerShape(),
-
+            dividerShape: _DividerShape(),
             thumbIcon: Icon(
               Icons.circle,
               size: 20,
               color: thumbColor,
             ),
-            thumbShape: const SfThumbShape(),
+            thumbShape:  _SfThumbShape(),
+
           ),
         ),
         SizedBox(
@@ -84,14 +88,96 @@ class _AppRangePickerState extends State<AppRangePicker> {
               widget.onChanged(
                 double.parse(myValue.toStringAsFixed(2)),
               );
-              selectedValue = double.parse(
-                myValue.toStringAsFixed(2),
-              );
+              selectedValue = double.parse(myValue.toStringAsFixed(2));
             },
             controller: TextEditingController(text: selectedValue.toString()),
           ),
         ),
       ],
     );
+  }
+}
+
+class _SfTrackShape extends SfTrackShape {
+  @override
+  void paint(PaintingContext context, Offset offset, Offset? thumbCenter,
+      Offset? startThumbCenter, Offset? endThumbCenter,
+      {required RenderBox parentBox,
+        required themeData,
+        SfRangeValues? currentValues,
+        dynamic currentValue,
+        required Animation<double> enableAnimation,
+        required Paint? inactivePaint,
+        required Paint? activePaint,
+        required TextDirection textDirection}) {
+    Paint paint = Paint()
+      ..color = themeData.activeTrackColor!
+      ..style = PaintingStyle.fill
+      ..strokeWidth = 0;
+    super.paint(context, offset, thumbCenter, startThumbCenter, endThumbCenter,
+        parentBox: parentBox,
+        themeData: themeData,
+        enableAnimation: enableAnimation,
+        inactivePaint: inactivePaint,
+        activePaint: paint,
+        textDirection: textDirection);
+  }
+}
+
+class _SfThumbShape extends SfThumbShape {
+  @override
+  void paint(PaintingContext context, Offset center,
+      {required RenderBox parentBox,
+        required RenderBox? child,
+        required themeData,
+        SfRangeValues? currentValues,
+        dynamic currentValue,
+        required Paint? paint,
+        required Animation<double> enableAnimation,
+        required TextDirection textDirection,
+        required SfThumb? thumb}) {
+    final Path path = Path();
+
+    path.moveTo(center.dx, center.dy);
+    path.lineTo(center.dx + 10, center.dy - 15);
+    path.lineTo(center.dx - 10, center.dy - 15);
+    path.close();
+    context.canvas.drawPath(
+        path,
+        Paint()
+          ..color = themeData.thumbColor!
+          ..style = PaintingStyle.fill
+          ..strokeWidth = 2);
+  }
+}
+
+class _DividerShape extends SfDividerShape {
+  @override
+  void paint(PaintingContext context, Offset center, Offset? thumbCenter,
+      Offset? startThumbCenter, Offset? endThumbCenter,
+      {required RenderBox parentBox,
+        required themeData,
+        SfRangeValues? currentValues,
+        dynamic currentValue,
+        required Paint? paint,
+        required Animation<double> enableAnimation,
+        required TextDirection textDirection}) {
+    bool isActive;
+
+    switch (textDirection) {
+      case TextDirection.ltr:
+        isActive = center.dx <= thumbCenter!.dx;
+        break;
+      case TextDirection.rtl:
+        isActive = center.dx >= thumbCenter!.dx;
+        break;
+    }
+
+    context.canvas.drawRect(
+        Rect.fromCenter(center: center, width: 2.0, height: 5.0),
+        Paint()
+          ..isAntiAlias = true
+          ..style = PaintingStyle.fill
+          ..color = isActive ? themeData.activeTrackColor! : Colors.white);
   }
 }

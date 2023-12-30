c304274cd326a8f74f317a0a38f8ce3f82413e81
Asfar Ali <30603432+s4-ali@users.noreply.github.com>
2023-12-30 05:10:46 +0500

    #88: updated list field

--- lib/widgets/fields/list_field.dart
+++ lib/widgets/fields/list_field.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quest/widgets/core/property.dart';
 import 'package:flutter_quest/widgets/core/property_previewer.dart';
+import 'package:flutter_quest/widgets/dropdown_button.dart';
 
 class ListField<T> extends PropertyWidget<T> {
   final List<T> values;
@@ -16,51 +17,23 @@ class ListField<T> extends PropertyWidget<T> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 44,
-      child: DropdownButtonFormField<T>(
-        value: value,
-        alignment: AlignmentDirectional.center,
-        icon: const SizedBox(),
-        borderRadius: BorderRadius.circular(8),
-        decoration: const InputDecoration(
-          filled: true,
-          isDense: true,
-          contentPadding: EdgeInsets.only(left: 14, top: 12, bottom: 12),
-          fillColor: Color(0xFF36343B),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(8),
-            ),
-            borderSide: BorderSide(
-              color: Color(0xFF3772FF),
-            ),
+    return AppDropDownButton<T>(
+      selectedOption: value,
+      items: values.map((T item) {
+        return DropdownMenuItem<T>(
+          value: item,
+          enabled: true,
+          child: Text(
+            titleBuilder == null ? item.toString() : titleBuilder!(item),
           ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(8),
-            ),
-            borderSide: BorderSide(
-              color: Color(0xFFF2F4F7),
-              width: 1,
-            ),
-          ),
-        ),
-        items: values.map((T item) {
-          return DropdownMenuItem<T>(
-            value: item,
-            enabled: true,
-            child: Text(
-              titleBuilder == null ? item.toString() : titleBuilder!(item),
-            ),
-          );
-        }).toList(),
-        onChanged: (val) {
-          if (val != null) {
-            onChanged(val);
-          }
-        },
-      ),
+        );
+      }).toList(),
+      onChanged: (val) {
+        if (val != null) {
+          onChanged(val);
+        }
+      },
+      width: double.maxFinite,
     );
   }
 }
@@ -70,14 +43,34 @@ class ListPreviewer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PropertyPreviewer<List<String>>(
+    return PropertyPreviewer<String>(
       title: "List",
-      values: const [["ABC", "DEF", "GHI"], ["JKL", "MNO", "PQR"], ["RST", "UVW", "XYZ"]],
+      values: const [
+        "ABC",
+        "DEF",
+        "GHI",
+        "JKL",
+        "MNO",
+        "PQR",
+        "RST",
+        "UVW",
+        "XYZ"
+      ],
       propertyBuilder: (onChanged, value) {
         return ListField(
           onChanged: onChanged,
           value: value,
-          values: const [["ABC", "DEF", "GHI"], ["JKL", "MNO", "PQR"], ["RST", "UVW", "XYZ"]],
+          values: const [
+            "ABC",
+            "DEF",
+            "GHI",
+            "JKL",
+            "MNO",
+            "PQR",
+            "RST",
+            "UVW",
+            "XYZ"
+          ],
         );
       },
     );
